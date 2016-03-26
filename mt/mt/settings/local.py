0879db291a31936526fd9f48d39da97943b2b727
Ronald Kwan <rkwan@berkeley.edu>
2016-03-26 03:22:05 -0700

    add django-debug-toolbar

--- mt/mt/settings/local.py
+++ mt/mt/settings/local.py
@@ -2,4 +2,7 @@ from .base import *
 
 DEBUG = True
 
+THIRD_PARTY_APPS += ['debug_toolbar']
+INSTALLED_APPS = DJANGO_CORE_APPS + THIRD_PARTY_APPS + CUSTOM_APPS
+
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
